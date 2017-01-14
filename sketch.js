1d874108afc888c9be89cf9826172f8be017d8af
DerMagsTa <mlanzrath@outlook.de>
2017-01-14 21:41:05 +0100

    HSB Matrix mit Punkten

--- sketch.js
+++ sketch.js
@@ -1,33 +1,40 @@
-var mausGedrückt = false;
-var kreis {
-  x: mouseX,
-  y: mouseY,
-  durchmesser: 100,
-  farbe: RGB(0,255,0)
+var kreis = {
+  x: 0,
+  y: 0,
+  durchmesser: 10,
+  r: 0,
+  g: 255,
+  b: 0,
+  h: 0,
+  s: 0
 };
+var positionen = [];
+var pos = 0;
 
 function setup() {
   createCanvas(600, 400);
-  background(250, 250, 100);
+  background(0, 0, 0);
+  colorMode(HSB);
+
+  for (var row = 0; row <= height; row += 10) {
+    for (var col = 0; col <= width; col += 10) {
+      positionen.push(createVector(col, row));
+    }
+  }
 }
 
 function draw() {
 
-  if (mausGedrückt === true) {
-    //Kreis
-    fill(kreis.farbe);
+  if (pos < positionen.length) {
+    kreis.x = positionen[pos].x;
+    kreis.y = positionen[pos].y;
+    kreis.h = map(kreis.x, 0, width, 0, 255);
+    kreis.s = map(kreis.y, 0, height, 0, 255);
+
+    fill(kreis.h, kreis.s, 100);
     noStroke();
     ellipse(kreis.x, kreis.y, kreis.durchmesser, kreis.durchmesser);
-  }
-  //Viereck
-  fill(200, 250, 200);
-  rect(400, 100, 50, 50);
-}
 
-function mousePressed() {
-  if (mausGedrückt) {
-    mausGedrückt = false;
-  } else {
-    mausGedrückt = true;
   }
+  pos = pos + 1;
 }
